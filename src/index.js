1f2296f93b4e4a59e07e9317d5583d4db63c146c
Vishal Sharma <vsharma2473@gmail.com>
2023-11-24 08:37:24 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,44 @@
+// require('dotenv').config({path:'./env'})
+import dotenv from 'dotenv'
+import connectDB from './db/index.js'
+import { app } from './app.js'
+dotenv.config({
+    path:'./env'
+})
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at port: ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("MONGO db connection failed !!!",err)
+})
+
+
+
+
+
+
+
+
+
+
+// ( async () => {
+//     try{
+//         await mongoose.connect(`${process.env.MONGODB_URL}/${DBNAME}`)
+//         app.on("error",()=>{
+//             console.log("ERROR: ",error)
+//             throw error
+//         })
+
+//         app.listen(proces.env.PORT, ()=>{
+//             console.log(`App is listening on port ${process.env.PORT}`)
+//         })
+
+//     }catch(error){
+//         console.log("ERROR: ",error)
+//         throw err
+//     }
+// })()
